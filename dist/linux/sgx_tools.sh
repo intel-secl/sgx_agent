4be9c9ce355239d139b88eef9290776ae8582316
Raviraj Bhat <raviraj.bhat@intel.com>
2020-07-08 19:44:52 -0700

    Use DCAP 1.7

--- dist/linux/sgx_tools.sh
+++ dist/linux/sgx_tools.sh
@@ -1,57 +1,40 @@
 #!/bin/bash
-
+DCAP_VERSION=1.7
+SGX_DCAP_TAG=DCAP_1.7
+OS_FLAVOUR="rhel8.1-server"
+MULTIPACKAGE_AGENT_RPM="https://download.01.org/intel-sgx/sgx-dcap/$DCAP_VERSION/linux/tools/SGXMultiPackageAgent/$OS_FLAVOUR"
 SGX_DCAP_REPO="https://github.com/intel/SGXDataCenterAttestationPrimitives.git"
 GIT_CLONE_PATH=/tmp/dataCenterAttestationPrimitives
-SGX_DCAP_RPM_VER=1.6.90.2-1
-SGX_DCAP_TAG=DCAP_1.6
+MP_RPM_VER=1.7.90.2-1
 
 install_sgx_components()
 {
 	#install msr-tools
-	which rdmsr
-	x=`echo $?`
-	if [ $x -ne 0 ]
-	then
+	if [ ! -f /usr/sbin/rdmsr ]; then
 		yum localinstall -y http://rpmfind.net/linux/fedora/linux/releases/30/Everything/x86_64/os/Packages/m/msr-tools-1.3-11.fc30.x86_64.rpm
-	else
-		echo "rdmsr present. Continuing...."
 	fi
 	rm -rf $GIT_CLONE_PATH
 
-	mkdir -p $GIT_CLONE_PATH
-	pushd  $GIT_CLONE_PATH
+	# remove_pccs_connect.diff disables access to PCCS
 	echo "Please provide patch file path"
 	read path
-	echo $path
-	ls $path/remove_pccs_connect.diff
-	status=$?
-	if test $status -ne 0
-	then
+	if [ ! -f $path/remove_pccs_connect.diff ]; then
 		echo "file not found on the given path"
 		exit 1
 	fi
 
+	# install uefi rpm to extract manifest file
+	rpm -ivh $MULTIPACKAGE_AGENT_RPM/libsgx-ra-uefi-$MP_RPM_VER.el8.x86_64.rpm
+
+	# build and  install PCKidretrieval tool
 	git clone $SGX_DCAP_REPO $GIT_CLONE_PATH/
+	cd $GIT_CLONE_PATH/
 	git checkout $SGX_DCAP_TAG
 	cp $path/remove_pccs_connect.diff $GIT_CLONE_PATH/
-
-	#Build Registration Service
-	cd $GIT_CLONE_PATH/tools/SGXPlatformRegistration
-	source /opt/intel/sgxsdk/environment
-	make clean || exit 1
-	make || exit 1
-	make rpm_pkg
-	cd ./build/installer
-	rpm -e libsgx-ra-uefi-devel-${SGX_DCAP_RPM_VER}.el8.x86_64 libsgx-ra-uefi-${SGX_DCAP_RPM_VER}.el8.x86_64 libsgx-ra-network-devel-${SGX_DCAP_RPM_VER}.el8.x86_64 libsgx-ra-network-${SGX_DCAP_RPM_VER}.el8.x86_64
-	rpm -ivh libsgx-ra-uefi-devel-${SGX_DCAP_RPM_VER}.el8.x86_64.rpm libsgx-ra-network-${SGX_DCAP_RPM_VER}.el8.x86_64.rpm libsgx-ra-uefi-${SGX_DCAP_RPM_VER}.el8.x86_64.rpm
-
-	#Build PCKRetrievalTool
 	cd $GIT_CLONE_PATH/tools/PCKRetrievalTool
 	git apply $path/remove_pccs_connect.diff
-	make clean || exit 1
-	make MPA=1 || exit 1
-	cp -u libdcap_quoteprov.so.1 enclave.signed.so /
-	cp -u PCKIDRetrievalTool /usr/sbin/
+	make
+	cp -u libdcap_quoteprov.so.1 pck_retrieve_tool_enclave.signed.so PCKIDRetrievalTool /usr/local/bin
 }
 
 install_sgx_components
