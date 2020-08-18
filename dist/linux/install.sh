7884aabc7ece82d56120a3cd8f4d26c55e5ca0da
Raviraj Bhat <raviraj.bhat@intel.com>
2020-08-17 23:52:06 -0700

    ISECL-10889:misconfiguration: excessive file permission assigned to the /var/log/ folders of SHUB SHVS, and SQVS respectively

--- dist/linux/install.sh
+++ dist/linux/install.sh
@@ -64,7 +64,7 @@ chmod g+s $CONFIG_PATH/root-ca
 
 # Create logging dir in /var/log
 mkdir -p $LOG_PATH && chown sgx_agent:sgx_agent $LOG_PATH
-chmod 755 $LOG_PATH
+chmod 700 $LOG_PATH
 chmod g+s $LOG_PATH
 
 # Install systemd script
