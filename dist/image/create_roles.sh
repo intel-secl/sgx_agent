836d409bd4acf6c18661386a271c237eb0132814
Rashmi Rajashekar <rashmi.rajashekara@intel.com>
2021-03-10 02:36:29 -0800

    updated create roles script for containers

--- dist/image/create_roles.sh
+++ dist/image/create_roles.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+source agent.conf
+if [ $? -ne 0 ]; then
+        echo "${red} please set correct values in agent.conf ${reset}"
+        exit 1
+fi
+
+AAS_BASE_URL=https://$AAS_IP:8444/aas/v1
 CURL_OPTS="-s -k"
 CONTENT_TYPE="Content-Type: application/json"
 ACCEPT="Accept: application/jwt"
@@ -10,16 +21,19 @@ tmpdir=$(mktemp -d -p /tmp/sgx_agent)
 #Get the CSP Admin JWT Token
 ADMIN_TOKEN=`curl $CURL_OPTS -H "$CONTENT_TYPE" -H "$ACCEPT" -X POST $AAS_BASE_URL/token -d \{\"username\":\"$CSP_ADMIN_USERNAME\",\"password\":\"$CSP_ADMIN_PASSWORD\"\}`
 if [ $? -ne 0 ]; then
-        echo "failed to get csp admin token"
+        echo "${red} failed to get csp admin token ${reset}"
         exit 1
 fi
 
 HW_UUID=`dmidecode -s system-uuid`
 
+SECONDS_PER_DAY=86400
+VALIDITY_SECONDS=$(( VALIDITY_DAYS * SECONDS_PER_DAY ))
+
 cat > $tmpdir/request_data.json << EOF
 {
     "subject": "$HW_UUID",
-    "validity_seconds": 2592000,
+    "validity_seconds": $VALIDITY_SECONDS,
     "claims": {
         "roles": [{
             "service": "SCS",
@@ -39,7 +53,10 @@ EOF
 
 LONG_LIVED_TOKEN=`curl $CURL_OPTS -H "$CONTENT_TYPE" -H "$ACCEPT" -H "Authorization: Bearer $ADMIN_TOKEN" -X POST $AAS_BASE_URL/custom-claims-token -d @$tmpdir/request_data.json`
 if [ $? -ne 0 ]; then
-        echo "failed to get long-lived token"
+        echo "${red} failed to get long-lived token ${reset}"
         exit 1
 fi
 echo $LONG_LIVED_TOKEN
+
+# cleanup
+rm -rf $tmpdir
